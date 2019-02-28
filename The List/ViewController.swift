b194e3ccca367da1d5e78d09f80ed085b792e169
Pandu <pandurang.yachwad@gmail.com>
2019-02-27 22:33:35 -0600

    Initial Commit

---
+++ The List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The List
+//
+//  Created by Pandu on 2/27/19.
+//  Copyright © 2019 123 Apps Studio LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
